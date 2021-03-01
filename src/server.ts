3da419e13366827aa23fdb6cecfca763dedb283f
pedrohrocodes <pedrohrocodes@gmail.com>
2021-02-28 22:46:30 -0300

    :sparkles: Aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,12 @@
+import 'reflect-metadata'
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-// GET => Busca
-// POST => Salvar
-// PUT => Alterar
-// DELETE => Deletar
-// PATCH => Alteracao Especifica
+app.use(express.json());
 
-// http://localhost:3333/users
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World - NLW04" });
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => request, response
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running!"));
